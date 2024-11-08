0b6173419a523119edcf46ef00193c1f292a9c36
Chris <HernandezMirandaChris@gmail.com>
2024-11-07 23:04:17 -0500

    Configure environment variables and database connection

--- server/models/db.mjs
+++ server/models/db.mjs
@@ -1,13 +1,12 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import { Pool } from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 
-
-const connectionString = process.env.DATABASE_URL || `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
+// Set up the pool using the DATABASE_URL environment variable for Heroku
 const pool = new Pool({
-  connectionString: connectionString,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,  // Enable SSL for Heroku
+  connectionString: process.env.DATABASE_URL, // Use DATABASE_URL set by Heroku
+  ssl: {
+    rejectUnauthorized: false, // Disable SSL verification for production
+  },
 });
 
 export default pool;
